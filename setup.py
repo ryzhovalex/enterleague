68e22e2c74a7b4f9843055dafc6cf40fc5b71c47
Alexander Ryzhov <thed4rkof@gmail.com>
2021-06-05 11:51:21 +0400

    Pre-release 0.2.0-proto.2

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@
 
 __author__ = "Alexander Ryzhov"
 __email__ = "thed4rkof@gmail.com"
-__version__ = "0.1.0-proto.1"
+__version__ = "0.2.0-proto.2"
 
 # here setup, manage and run operations will be defined
